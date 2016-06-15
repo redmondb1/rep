d38dd3eb446dc1a8f1862b16ca13b654739fee60
Brian Redmond <brianredmond@gmx.net>
2016-06-14 21:10:52 -0700

    Force keys to string
    Since comparisions of int and string will fail, it's safest to force the
    keys to always be string values even when they're numeric.

--- rep/rep_store.py
+++ rep/rep_store.py
@@ -8,13 +8,13 @@ class RepStore(collections.MutableMapping):
         self.update(dict(*args, **kwargs))
 
     def __getitem__(self, key):
-        return self.__store[key]
+        return self.__store[str(key)]
 
     def __setitem__(self, key, value):
-        self.__store[key] = value
+        self.__store[str(key)] = value
 
     def __delitem__(self, key):
-        del self.__store[key]
+        del self.__store[str(key)]
 
     def __iter__(self):
         return iter(self.__store)
@@ -23,7 +23,7 @@ class RepStore(collections.MutableMapping):
         return len(self.__store)
 
     def get_new_key(self):
-        return max(self.__store.keys(), key=int) + 1 if len(self.__store) else 1
+        return str(int(max(self.__store.keys(), key=int)) + 1) if len(self.__store) else '1'
 
     def create(self, value):
         new_key = self.get_new_key()
@@ -31,14 +31,14 @@ class RepStore(collections.MutableMapping):
         return new_key
 
     def read(self, key):
-        return self.__getitem__(key)
+        return self.__getitem__(str(key))
 
     def update(self, *args, **kwargs):
         for key, value in dict(*args, **kwargs).iteritems():
-            self.__store[key] = value
+            self.__store[str(key)] = value
 
     def delete(self, key):
-        self.__delitem__(key)
+        self.__delitem__(str(key))
 
     def as_dict(self):
         return {key: value.as_dict() for key, value in self.__store.iteritems()}
